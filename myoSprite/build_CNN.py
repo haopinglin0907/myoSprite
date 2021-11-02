c9f1f8b890a398d21fff66666efe2afad570cb11
haoping <haoping44556@gmail.com>
2021-11-02 15:14:28 +0800

    Updated build_CNN.py, the training takes the most recent 5 calibration data instead of only the recent one

--- myoSprite/build_CNN.py
+++ myoSprite/build_CNN.py
@@ -1,5 +1,6 @@
 #### PACKAGE IMPORTS ####
 import tensorflow as tf
+import glob2
 import os
 import numpy as np
 import pandas as pd
@@ -76,20 +77,63 @@ def make_keras_picklable():
     cls = Model
     cls.__reduce__ = __reduce__
 
-def train_CNN_model(emg_list, label_list):
+
+
+def mirror_sensor(df_unmirrored):
+    A_values = df_unmirrored.iloc[:, :8].values
+    A_mirrored = pd.DataFrame(A_values, columns=['c7', 'c6', 'c5', 'c4', 'c3', 'c2', 'c1', 'c8'])
+    A_mirrored = A_mirrored[['c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8']]
+    A_mirrored = pd.concat((A_mirrored, df_unmirrored.iloc[:, 8:].reset_index(drop = True)), axis = 1)
+
+    return A_mirrored
+def train_CNN_model(emg_list, label_list, side_list, ID):
 
     df = pd.DataFrame(np.array(emg_list).reshape(-1, 8), columns = ['c1', 'c2', 'c3', 'c4', 'c5', 'c7', 'c7', 'c8'])
+    df['side'] = np.array(side_list).reshape(-1, 1)
     df['Gesture'] = np.array(label_list).reshape(-1, 1)
 
-    X_train, y_train = split_sequence(df, window_size = 30, nonoverlap_size = 1)
+    if not os.path.exists(f'{ID}'):
+        os.mkdir(f'{ID}')
 
+    filename = glob2.glob(f'{ID}/emgRawData*')
+    if len(filename) != 0:
+        session = int(sorted(filename, key=os.path.getmtime)[-1].split('_')[1].split('.')[0])
+        df.to_pickle(f'{ID}/emgRawData_{session + 1}.pkl')
+    else:
+        df.to_pickle(f'{ID}/emgRawData_1.pkl')
+
+    # Training the model
+    df_all = pd.DataFrame(None)
+    for file in filename[-5:]:
+        df_all = pd.concat((df_all, pd.read_pickle(file)), axis = 0)
+
+    # Saving the gestureProfile
+    df_healthy = df_all[df_all['side'] == 'healthy']
+    df_healthy = mirror_sensor(df_healthy)
+
+    gestureProfile = []
+    for gesture in df_healthy['Gesture'].unique():
+        A = df_healthy[df_healthy['Gesture'] == gesture].iloc[:, :8]
+        gestureProfile.append({'Gesture': gesture,
+                               'RMS': np.sqrt(np.mean(A ** 2, axis=0)).values})
+    gestureProfile = pd.DataFrame(gestureProfile)
+    gestureProfile.to_pickle(f'{ID}/gestureProfile_{ID}.pkl')
+
+    df_training = df_all[df_all['side'] == 'training']
+    df_training.reset_index(inplace=True, drop=True)
+    df_temp = df_training.groupby('Gesture').head(200)
+
+    df_training.loc[df_temp.index, :] = np.nan
+    df_training.dropna(inplace=True)
+
+    X_train, y_train = split_sequence(df_training, window_size = 30, nonoverlap_size = 1)
     X_train = X_train.reshape((-1, X_train.shape[1], X_train.shape[2], 1))
     le = LabelEncoder()
     y_train = le.fit_transform(y_train)
     y_train = to_categorical(y_train)
 
     reduce_lr = ReduceLROnPlateau(monitor='acc', patience=3, mode='max', verbose=0)
-    early_stopping = EarlyStopping(monitor='acc', patience=10, mode='max', verbose=0)
+    early_stopping = EarlyStopping(monitor='acc', patience=5, mode='max', verbose=0)
 
     model = get_CNN_model(X_train.shape, y_train.shape[1], wd = 0.01)
     model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
@@ -98,11 +142,11 @@ def train_CNN_model(emg_list, label_list):
 
     model.fit(X_train, y_train,
               callbacks=[reduce_lr, early_stopping],
-              epochs=30, verbose=1, batch_size = 128)
+              epochs=30, verbose=1, batch_size = 256)
 
     # Run the function
     make_keras_picklable()
 
     # Save
-    with open('model.pkl', 'wb') as f:
+    with open(f'{ID}/model.pkl', 'wb') as f:
         pickle.dump(model, f)
